e65de64f97ae8bf5499586d1a54fcea17b9600cf
zsoldosp <peter.zsoldos@gmail.com>
2014-05-16 15:48:29 +0200

    Extracted test_querysets_checking_for_membership

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -117,11 +117,19 @@ class TemporaryClassSplittingUpObjectCreationTest(TestCase):
         self.assertTrue(Article.objects.get(id__exact=a8.id) != Article.objects.get(id__exact=a7.id))
         self.assertFalse(Article.objects.get(id__exact=a8.id) == Article.objects.get(id__exact=a7.id))
 
-        # You can use 'in' to test for membership...
-        self.assertTrue(a8 in Article.objects.all())
+    def test_querysets_checking_for_membership(self):
+        headlines = [
+            'Area man programs in Python', 'Second article', 'Third article']
+        some_pub_date = datetime(2014, 5, 16, 12, 1)
+        for headline in headlines:
+            Article(headline=headline, pub_date=some_pub_date).save()
+        a = Article(headline='Some headline', pub_date=some_pub_date)
+        a.save()
 
+        # You can use 'in' to test for membership...
+        self.assertTrue(a in Article.objects.all())
         # ... but there will often be more efficient ways if that is all you need:
-        self.assertTrue(Article.objects.filter(id=a8.id).exists())
+        self.assertTrue(Article.objects.filter(id=a.id).exists())
 
     def test_datetimes_returns_available_dates_for_given_scope_and_given_field(self):
         pub_dates = [
