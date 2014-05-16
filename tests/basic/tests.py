22b82f86cc36c7897e1c036936ffeb1d9f8f1137
zsoldosp <peter.zsoldos@gmail.com>
2014-05-16 17:05:33 +0200

    moved test method to appropriate TestCase class

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -124,18 +124,6 @@ class TemporaryClassSplittingUpObjectCreationTest(TestCase):
         a.save()
         self.assertEqual(a.id, current_id)
 
-    def test_not_equal_and_equal_operators_behave_as_expected_on_instances(self):
-        some_pub_date = datetime(2014, 5, 16, 12, 1)
-        a1 = Article.objects.create(headline='First', pub_date=some_pub_date)
-        a2 = Article.objects.create(headline='Second', pub_date=some_pub_date)
-        # TODO: inconsistent assertions below
-        self.assertTrue(a1 != a2)
-        self.assertFalse(a1 == a2)
-        self.assertEqual(a1, Article.objects.get(id__exact=a1.id))
-
-        self.assertTrue(Article.objects.get(id__exact=a1.id) != Article.objects.get(id__exact=a2.id))
-        self.assertFalse(Article.objects.get(id__exact=a2.id) == Article.objects.get(id__exact=a1.id))
-
     def test_querysets_checking_for_membership(self):
         headlines = [
             'Area man programs in Python', 'Second article', 'Third article']
@@ -299,6 +287,18 @@ class TemporaryClassSplittingUpObjectCreationTest(TestCase):
 
 
 class ModelTest(TestCase):
+
+    def test_not_equal_and_equal_operators_behave_as_expected_on_instances(self):
+        some_pub_date = datetime(2014, 5, 16, 12, 1)
+        a1 = Article.objects.create(headline='First', pub_date=some_pub_date)
+        a2 = Article.objects.create(headline='Second', pub_date=some_pub_date)
+        # TODO: inconsistent assertions below
+        self.assertTrue(a1 != a2)
+        self.assertFalse(a1 == a2)
+        self.assertEqual(a1, Article.objects.get(id__exact=a1.id))
+
+        self.assertTrue(Article.objects.get(id__exact=a1.id) != Article.objects.get(id__exact=a2.id))
+        self.assertFalse(Article.objects.get(id__exact=a2.id) == Article.objects.get(id__exact=a1.id))
 
     def test_lookup(self):
         # No articles are in the system yet.
