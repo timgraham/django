aa7b8f555404caa461fad69ed23d159564b2f2cf
zsoldosp <peter.zsoldos@gmail.com>
2014-05-16 15:51:56 +0200

    Extracted test_not_equal_and_equal_operators_behave_as_expected_on_instances

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -109,13 +109,17 @@ class TemporaryClassSplittingUpObjectCreationTest(TestCase):
         a8.save()
         self.assertEqual(a8.id, current_id)
 
-        # Check that != and == operators behave as expecte on instances
-        self.assertTrue(a7 != a8)
-        self.assertFalse(a7 == a8)
-        self.assertEqual(a8, Article.objects.get(id__exact=a8.id))
+    def test_not_equal_and_equal_operators_behave_as_expected_on_instances(self):
+        some_pub_date = datetime(2014, 5, 16, 12, 1)
+        a1 = Article.objects.create(headline='First', pub_date=some_pub_date)
+        a2 = Article.objects.create(headline='Second', pub_date=some_pub_date)
+        # TODO: inconsistent assertions below
+        self.assertTrue(a1 != a2)
+        self.assertFalse(a1 == a2)
+        self.assertEqual(a1, Article.objects.get(id__exact=a1.id))
 
-        self.assertTrue(Article.objects.get(id__exact=a8.id) != Article.objects.get(id__exact=a7.id))
-        self.assertFalse(Article.objects.get(id__exact=a8.id) == Article.objects.get(id__exact=a7.id))
+        self.assertTrue(Article.objects.get(id__exact=a1.id) != Article.objects.get(id__exact=a2.id))
+        self.assertFalse(Article.objects.get(id__exact=a2.id) == Article.objects.get(id__exact=a1.id))
 
     def test_querysets_checking_for_membership(self):
         headlines = [
