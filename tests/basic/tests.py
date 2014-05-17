21f834be49e6fb6a7beb64d36405d10d82a5575b
zsoldosp <peter.zsoldos@gmail.com>
2014-05-17 10:46:04 +0200

    Formatted docstring comments
    as per @Markush2010 suggestions

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -33,8 +33,10 @@ class ModelInstanceCreationTests(TestCase):
         self.assertEquals(1, Article.objects.all().count())
 
     def test_can_initialize_model_instance_using_positional_arguments(self):
-        """You can initialize a model instance using positional arguments,
-           which should match the field order as defined in the model."""
+        """
+        You can initialize a model instance using positional arguments,
+        which should match the field order as defined in the model.
+        """
         a2 = Article(None, 'Second article', datetime(2005, 7, 29))
         a2.save()
 
@@ -78,8 +80,10 @@ class ModelInstanceCreationTests(TestCase):
         )
 
     def test_can_leave_off_value_for_autofield_and_it_gets_value_on_save(self):
-        """You can leave off the value for an AutoField when creating an
-           object, because it'll get filled in automatically when you save()."""
+        """
+        You can leave off the value for an AutoField when creating an
+        object, because it'll get filled in automatically when you save().
+        """
         a5 = Article(headline='Article 5', pub_date=datetime(2005, 7, 31))
         a5.save()
         self.assertEqual(a5.headline, 'Article 5')
