387cc40f029c0f0322968b755f6ff62e220aa31a
Tim Graham <timograham@gmail.com>
2020-08-24 21:08:09 -0400

    adapt tests for lack of foreign key indexes in CockraochDB 20.2

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -284,7 +284,8 @@ class SchemaTests(TransactionTestCase):
         with connection.schema_editor() as editor:
             editor.add_field(Node, new_field)
             editor.execute('UPDATE schema_node SET new_parent_fk_id = %s;', [parent.pk])
-        self.assertIn('new_parent_fk_id', self.get_indexes(Node._meta.db_table))
+        if connection.features.indexes_foreign_keys:
+            self.assertIn('new_parent_fk_id', self.get_indexes(Node._meta.db_table))
 
     @skipUnlessDBFeature(
         'can_create_inline_fk',
@@ -1152,6 +1153,7 @@ class SchemaTests(TransactionTestCase):
             editor.create_model(Author)
             editor.create_model(Book)
         expected_fks = 1 if connection.features.supports_foreign_keys else 0
+        expected_indexes = 1 if connection.features.indexes_foreign_keys else 0
 
         # Check the index is right to begin with.
         counts = self.get_constraints_count(
@@ -1159,7 +1161,7 @@ class SchemaTests(TransactionTestCase):
             Book._meta.get_field('author').column,
             (Author._meta.db_table, Author._meta.pk.column),
         )
-        self.assertEqual(counts, {'fks': expected_fks, 'uniques': 0, 'indexes': 1})
+        self.assertEqual(counts, {'fks': expected_fks, 'uniques': 0, 'indexes': expected_indexes})
 
         old_field = Book._meta.get_field('author')
         new_field = OneToOneField(Author, CASCADE)
@@ -1180,6 +1182,7 @@ class SchemaTests(TransactionTestCase):
             editor.create_model(Author)
             editor.create_model(Book)
         expected_fks = 1 if connection.features.supports_foreign_keys else 0
+        expected_indexes = 1 if connection.features.indexes_foreign_keys else 0
 
         # Check the index is right to begin with.
         counts = self.get_constraints_count(
@@ -1187,7 +1190,7 @@ class SchemaTests(TransactionTestCase):
             Book._meta.get_field('author').column,
             (Author._meta.db_table, Author._meta.pk.column),
         )
-        self.assertEqual(counts, {'fks': expected_fks, 'uniques': 0, 'indexes': 1})
+        self.assertEqual(counts, {'fks': expected_fks, 'uniques': 0, 'indexes': expected_indexes})
 
         old_field = Book._meta.get_field('author')
         # on_delete changed from CASCADE.
@@ -1202,7 +1205,7 @@ class SchemaTests(TransactionTestCase):
             (Author._meta.db_table, Author._meta.pk.column),
         )
         # The index remains.
-        self.assertEqual(counts, {'fks': expected_fks, 'uniques': 0, 'indexes': 1})
+        self.assertEqual(counts, {'fks': expected_fks, 'uniques': 0, 'indexes': expected_indexes})
 
     def test_alter_field_o2o_to_fk(self):
         with connection.schema_editor() as editor:
@@ -2512,12 +2515,14 @@ class SchemaTests(TransactionTestCase):
             with self.assertRaisesMessage(TransactionManagementError, message):
                 editor.execute(editor.sql_create_table % {'table': 'foo', 'definition': ''})
 
-    @skipUnlessDBFeature('supports_foreign_keys')
+    @skipUnlessDBFeature('supports_foreign_keys', 'indexes_foreign_keys')
     def test_foreign_key_index_long_names_regression(self):
         """
         Regression test for #21497.
         Only affects databases that supports foreign keys.
         """
+        if not connection.features.indexes_foreign_keys:
+            self.skipTest('Foreign keys not indexed.')
         # Create the table
         with connection.schema_editor() as editor:
             editor.create_model(AuthorWithEvenLongerName)
