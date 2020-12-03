ac57b517bb991493e9997ea7671eac299a1051dc
Tim Graham <timograham@gmail.com>
2020-12-03 13:14:45 -0500

    update expected query counts for enable_experimental_alter_column_type_general

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -2527,7 +2527,9 @@ class SchemaTests(TransactionTestCase):
             with connection.schema_editor() as editor:
                 editor.alter_field(Author, Author._meta.get_field("name"), new_field)
         # One SQL statement is executed to alter the field.
-        self.assertEqual(len(cm.records), 1)
+        # Plus another SQL statement on CockroachDB for
+        # enable_experimental_alter_column_type_general.
+        self.assertEqual(len(cm.records), 2 if connection.vendor == 'cockroachdb' else 1)
 
     @isolate_apps("schema")
     def test_unique_and_reverse_m2m(self):
@@ -2560,7 +2562,9 @@ class SchemaTests(TransactionTestCase):
             with connection.schema_editor() as editor:
                 editor.alter_field(Tag, Tag._meta.get_field("slug"), new_field)
         # One SQL statement is executed to alter the field.
-        self.assertEqual(len(cm.records), 1)
+        # Plus another SQL statement on CockroachDB for
+        # enable_experimental_alter_column_type_general.
+        self.assertEqual(len(cm.records), 2 if connection.vendor == 'cockroachdb' else 1)
         # Ensure that the field is still unique.
         Tag.objects.create(title="foo", slug="foo")
         with self.assertRaises(IntegrityError):
