55f87bf96058e3d0f083befe1ca47ad0bb0dbdd5
Tim Graham <timograham@gmail.com>
2022-11-01 09:28:44 -0400

    dbshell: pass ssl parameters in connection URL instead of using --certs-dir and --insecure

--- tests/dbshell/test_roach.py
+++ tests/dbshell/test_roach.py
@@ -14,7 +14,7 @@ class DbshellTests(SimpleTestCase):
             self.settings_to_cmd_args_env({})
 
     def test_basic(self):
-        expected_args = ["cockroach", "sql", "--insecure"]
+        expected_args = ["cockroach", "sql"]
         args, env = self.settings_to_cmd_args_env(
             {
                 "NAME": "somedbname",
@@ -28,11 +28,12 @@ class DbshellTests(SimpleTestCase):
         self.assertEqual(args, expected_args)
         self.assertEqual(
             env['COCKROACH_URL'],
-            'postgresql://someuser:somepassword@somehost:444/somedbname'
+            'postgresql://someuser:somepassword@somehost:444/somedbname?'
+            'sslmode=disable'
         )
 
     def test_extra_args(self):
-        expected_args = ["cockroach", "sql", "--insecure", "--extra"]
+        expected_args = ["cockroach", "sql", "--extra"]
         args, env = self.settings_to_cmd_args_env(
             {
                 "NAME": "somedbname",
@@ -47,11 +48,12 @@ class DbshellTests(SimpleTestCase):
         self.assertEqual(args, expected_args)
         self.assertEqual(
             env['COCKROACH_URL'],
-            'postgresql://someuser:somepassword@somehost:444/somedbname'
+            'postgresql://someuser:somepassword@somehost:444/somedbname?'
+            'sslmode=disable'
         )
 
     def test_no_password_or_port(self):
-        expected_args = ["cockroach", "sql", "--insecure"]
+        expected_args = ["cockroach", "sql"]
         args, env = self.settings_to_cmd_args_env(
             {
                 "NAME": "somedbname",
@@ -65,7 +67,7 @@ class DbshellTests(SimpleTestCase):
         self.assertEqual(args, expected_args)
         self.assertEqual(
             env['COCKROACH_URL'],
-            'postgresql://someuser:@somehost:/somedbname'
+            'postgresql://someuser:@somehost:/somedbname?sslmode=disable'
         )
 
     def test_sslrootcert(self):
@@ -77,14 +79,17 @@ class DbshellTests(SimpleTestCase):
                 "PASSWORD": "somepassword",
                 "HOST": "somehost",
                 "PORT": "444",
-                "OPTIONS": {'sslrootcert': 'path/to/ca.crt'},
+                "OPTIONS": {
+                    'sslmode': 'verify-full',
+                    'sslrootcert': 'path/to/ca.crt',
+                },
             }
         )
         self.assertEqual(args, expected_args)
         self.assertEqual(
             env['COCKROACH_URL'],
             'postgresql://someuser:somepassword@somehost:444/somedbname?'
-            'sslrootcert=path%2Fto%2Fca.crt'
+            'sslrootcert=path%2Fto%2Fca.crt&sslmode=verify-full'
         )
 
     def test_sslcert_and_sslkey(self):
@@ -97,6 +102,7 @@ class DbshellTests(SimpleTestCase):
                 "HOST": "somehost",
                 "PORT": "444",
                 "OPTIONS": {
+                    'sslmode': 'verify-full',
                     'sslcert': '/certs/client.myprojectuser.crt',
                     'sslkey': '/certs/client.myprojectuser.key',
                 },
@@ -107,7 +113,7 @@ class DbshellTests(SimpleTestCase):
             env['COCKROACH_URL'],
             'postgresql://someuser:somepassword@somehost:444/somedbname?'
             'sslcert=%2Fcerts%2Fclient.myprojectuser.crt&'
-            'sslkey=%2Fcerts%2Fclient.myprojectuser.key'
+            'sslkey=%2Fcerts%2Fclient.myprojectuser.key&sslmode=verify-full'
         )
 
     def test_sslmode_and_options(self):
@@ -133,7 +139,7 @@ class DbshellTests(SimpleTestCase):
         )
 
     def test_sslmode_disable(self):
-        expected_args = ["cockroach", "sql", "--insecure"]
+        expected_args = ["cockroach", "sql"]
         args, env = self.settings_to_cmd_args_env(
             {
                 "NAME": "somedbname",
